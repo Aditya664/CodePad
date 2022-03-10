427d1615902b01f964e0d3e12b86804b36a26619
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2022-03-10 12:19:38 +0530

    Update Codepad.py

--- Codepad.py
+++ Codepad.py
@@ -19,12 +19,26 @@ exit_icon = tk.PhotoImage(file="icons2/exit.png")
 
 file = tk.Menu(main_menu,tearoff=False)
 file.add_command(label='New', image = new_icon,compound = tk.LEFT,accelerator = 'Ctrl+n')
+file.add_command(label='Open', image = open_icon,compound = tk.LEFT,accelerator = 'Ctrl+o')
+file.add_command(label='Save', image = save_icon,compound = tk.LEFT,accelerator = 'Ctrl+s')
+file.add_command(label='Save as', image = saveas_icon,compound = tk.LEFT,accelerator = 'Ctrl+Alt+s')
+file.add_command(label='Exit', image = exit_icon,compound = tk.LEFT,accelerator = 'Ctrl+e')
+# Edit
 
-
-
-
+copy_icon = tk.PhotoImage(file = "icons2/copy.png")
+paste_icon = tk.PhotoImage(file = "icons2/paste.png")
+cut_icon = tk.PhotoImage(file = "icons2/cut.png")
+clr_icon = tk.PhotoImage(file = "icons2/clear_all.png")
+find_icon = tk.PhotoImage(file = "icons2/find.png")
 
 edit = tk.Menu(main_menu,tearoff=False)
+edit.add_command(label="Copy",image=copy_icon,compound=tk.LEFT,accelerator='Ctrl+c')
+edit.add_command(label="Paste",image=paste_icon,compound=tk.LEFT,accelerator='Ctrl+p')
+edit.add_command(label="Cut",image=cut_icon,compound=tk.LEFT,accelerator='Ctrl+C')
+edit.add_command(label="Clear All",image=clr_icon,compound=tk.LEFT,accelerator='Ctrl+Alt+x')
+edit.add_command(label="Find",image=find_icon,compound=tk.LEFT,accelerator='Ctrl+f')
+
+
 view = tk.Menu(main_menu,tearoff=False)
 color = tk.Menu(main_menu,tearoff=False)
 
